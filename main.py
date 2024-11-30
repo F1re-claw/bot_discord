3a3da3254698110235804c68cb20c5d6b7228797
F1re-claw <duchemin@et.esiea.fr>
2024-11-30 18:44:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -547,4 +547,4 @@ async def display_quest(ctx):
   await ctx.send(embed=embed)
 
 keep_alive()
-bot.run("MTMwODUxNjYwMDAwNTEzMjQxOA.GofXHP.04x2UtlLoZr-d3qr1a3C_7Hg1HucJzfdc6303w")
+bot.run("MTMwODUxNjYwMDAwNTEzMjQxOA.G7L237.1Xe0lmwoxwkEiatgeKtWeVl8mERIisszmYwgXk")
